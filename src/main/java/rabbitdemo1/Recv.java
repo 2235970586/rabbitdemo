53c255556d06168f9caaeb56297dee4c718072b5
2235970586 <2235970586@qq.com>
2019-01-18 14:29:17 +0800

    mq

---
+++ src/main/java/rabbitdemo1/Recv.java
@@ -0,0 +1,32 @@
+package rabbitdemo1;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.QueueingConsumer;
+
+public class Recv {
+	private final static String QUEUE_NAME = "q_test_02";
+
+	public static void main(String[] argv) throws Exception {
+
+		// 获取到连接以及mq通道
+		Connection connection = ConnectionUtil.getConnection();
+		// 从连接中创建通道
+		Channel channel = connection.createChannel();
+		// 声明队列
+		channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+
+		// 定义队列的消费者
+		QueueingConsumer consumer = new QueueingConsumer(channel);
+
+		// 监听队列
+		channel.basicConsume(QUEUE_NAME, true, consumer);
+
+		// 获取消息
+		while (true) {
+			QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+			String message = new String(delivery.getBody());
+			System.out.println(" [x] Received '" + message + "'");
+		}
+	}
+}
